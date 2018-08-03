c56397da22b30c819e46f634629de9d0ff2a3406
Yvonne <eysanders143@gmail.com>
2018-08-03 14:54:55 -0400

    created text processor class

---
+++ src/main/java/codeu/controller/TextProcessor.java
@@ -0,0 +1,9 @@
+package codeu.controller;
+package org.kefirsf.bb
+
+ public interface TextProcessor {
+	public CharSequence process(CharSequence source);
+    public String process(String source);
+    public StringBuilder process(StringBuilder source);
+    public StringBuffer process(StringBuffer source);
+}
